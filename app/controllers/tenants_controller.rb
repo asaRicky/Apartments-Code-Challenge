e3339697810543324991c9324f57faee913d6b13
Koome <judekoome360@gmail.com>
2023-06-29 16:19:15 +0300

    added Tenants

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,41 +1,48 @@
 class TenantsController < ApplicationController
-    before_action :set_tenant,only: [:show,:update,:destroy]
-    def index
-        @tenants=Tenant.all 
-        render json :@tenant
+  before_action :set_tenant, only: [:show, :update, :destroy]
+
+  def index
+    @tenants = Tenant.all
+    render json: @tenants
+  end
+
+  def show
+    render json: @tenant
+  end
+
+  def create
+    @tenant = Tenant.create(tenant_params)
+    if @tenant
+      render json: @tenant, status: :created
+    else
+      render json: @tenant.errors, status: :unprocessable_entity
     end
+  end
 
-    def show
-        render json:@tenant
+  def update
+    if @tenant.update(tenant_params)
+      render json: @tenant
+    else
+      render json: @tenant.errors, status: :unprocessable_entity
     end
+  end
 
-    def create
-        @tenant=Tenant.create(tenant_params)
-        if @tenant
-            render json :@tenant ,status :created
-        else render json :@tenant.errors,status: :unprocessable_entity
+  def destroy
+    @tenant.destroy
+    if @tenant.destroyed?
+      render json: { message: "Tenantenentst was successfully destroyed." }
+    else
+      render json: { message: "Tenant could not be destroyed." }, status: :unprocessable_entity
     end
+  end
 
-    def update
-        if @tenant.update(tenant_params)
-        render json :@tenant
-        else render json : @tenant.errors,status: :unprocessable_entity
-        end
+  private
 
-        def destroy
-            @tenant.destroy
-            if @tenant.destroyed?
-                render json:{message:"Tenant was successfully destroyed."}
-            else render json:{message:"Tenant could not be destroyed."}, status: :unprocessable_entity
-            end
-        end
-      private
-        def set_tenant
-            @tenant=Tenant.find(params[:id])
-        end
+  def set_tenant
+    @tenant = Tenant.find(params[:id])
+  end
 
-        def tenant_params
-            params.require(:tenant).permit(:name,:age)
-        end
-    end
+  def tenant_params
+    params.require(:tenant).permit(:name, :age)
+  end
 end
